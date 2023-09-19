62b1af5e037a2a91bab16742bdf8a669467fe69e
aimen114 <aimenzafar095@gmail.com>
2023-09-19 22:37:37 +0500

    added credentials

--- src/App.js
+++ src/App.js
@@ -4,6 +4,32 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project is created by{" "}
+        <a
+          href="https://unrivaled-gingersnap-27115c.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Aimen Zafar
+        </a>
+        , open sourced on{" "}
+        <a
+          href="https://github.com/aimen114/react-weather-app-2.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+        and Hosted on{" "}
+        <a
+          href="https://gentle-naiad-23c271.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }

522dc001b4b8e25522d349476fca2a56bc42800f
aimen114 <aimenzafar095@gmail.com>
2023-09-20 02:30:46 +0500

    added background for testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Weather from "./Weather.js";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <Weather />
       <footer>
         This project is created by{" "}
         <a
